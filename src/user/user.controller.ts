97ca9f1cf33d1a45f873bb14bb5c4760b1f2ecf9
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2022-05-25 19:52:28 +0100

    ⚡️ Circular dependency

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  public async getUserById(userId: string) {
+    return await this.userService.getUserById(userId);
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { FileModule } from '../file-service/file.module';
 import { FileService } from '../file-service/file.service';
 
 @Module({
